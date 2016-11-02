d32524595f8c053c65aa79f34f27fabb75efd217
apple <apple@kangshuangxideMacBook-Pro.local>
2016-11-02 09:25:22 +0800

    Initial Commit

---
+++ 布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  布局
+//
+//  Created by apple on 2016/11/2.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  布局
+//
+//  Created by apple on 2016/11/2.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
